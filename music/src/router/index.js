8d7a7fdd8f7da01c67e13900963087a99d404ee5
rjwx60 <614054722@qq.com>
2019-03-11 23:57:16 +0800

    add header

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Music from '@/components/Music'
+
+// 据说是为了解决以移动端点击 300 ms 延迟
+import fastclick from 'fastclick';
+
+fastclick.attach(document.body);
 
 Vue.use(Router)
 

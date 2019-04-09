c6ca98f5f214fb5a8d00d01e8ccf33275b24f377
rjwx60 <614054722@qq.com>
2019-04-09 14:58:49 +0800

    fisrt

---
+++ jklearn/05/learn/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <!-- 05 Test -->
+    <input type="text" v-model="inputInfo">
+    <button @click.prevent="addTodoItem">Add todoItem</button>
+    <test-item v-for="item in todoList" :key="item">
+      <!-- 插槽 - 插槽名 - 插槽接收值(对象) -->
+      <template v-slot:item="sonPropsObj">
+        <span :style="{fontSize: '16px', color: sonPropsObj.checked ? 'red' : 'grey'}">{{item}}</span>
+      </template>
+    </test-item>
+  </div>
+</template>
+
+<script>
+import TestItem from './components/TestItem.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TestItem
+  },
+  data() {
+    return {
+      inputInfo: '',
+      todoList: []
+    }
+  },
+  methods: {
+    addTodoItem() {
+      this.todoList.push(this.inputInfo)
+      this.inputInfo = ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ jklearn/05/learn/src/components/TestItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <li class="item">
+      <input type="checkbox" v-model='checked'>
+      <!-- 插槽 - 插槽名 - 插槽发送值 -->
+      <slot name="item" v-bind="{checked}"></slot>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  data() {
+    return{
+      checked: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .item {
+    color: orange;
+  }
+</style>
